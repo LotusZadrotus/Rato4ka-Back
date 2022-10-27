a3c5811f9cbd8984616f9a2ef1682d444a0ef30b
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-10-28 01:42:05 +0300

    Created a Service Exception to easily track errors.

---
+++ Rato4ka-back/Exceptions/ServiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Rato4ka_back.Exceptions
+{
+    public class ServiceException: Exception
+    {
+        private readonly string[] _params;
+        public ServiceException(string msg, params string[] param)
+            : base(msg)
+        {
+            _params = param;
+        }
+        public String GetDetailInfo()
+        {
+            var builder = new StringBuilder();
+            builder.AppendFormat("{0}{1}Additional parameters info:{2}", Message, Environment.NewLine, Environment.NewLine);
+            foreach(var param in _params)
+            {
+                builder.AppendLine(param);
+            }
+            return builder.ToString();
+        }
+    }
+}
