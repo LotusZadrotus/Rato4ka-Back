ac04c97e4ee380768cd173ee0a28947247762ede
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-10-28 01:40:03 +0300

    Created and implemented UOF structure.

---
+++ Rato4ka-back/Repositories/IUnit.cs
@@ -0,0 +1,14 @@
+﻿using Rato4ka_back.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Rato4ka_back.Repositories
+{
+    public interface IUnit: IDisposable
+    {
+        public IRepository<T> GetRepo<T>() where T : Base;
+        public int Save();
+        public Task<int> SaveAsync();
+        public Task<int?> GetIdByLink(string link);
+    }
+}
---
+++ Rato4ka-back/Repositories/impl/Unit.cs
@@ -0,0 +1,63 @@
+﻿using Rato4ka_back.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rato4ka_back.Repositories.impl
+{
+    public class Unit : IUnit
+    {
+        private readonly DBContext _dbContext;
+        public Unit(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IRepository<T> GetRepo<T>() where T : Base
+        {
+            return new Repository<T>(_dbContext);
+            
+        }
+
+        public int Save()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            var item = await _dbContext.SaveChangesAsync();
+            return item;
+        }
+        public Task<int?> GetIdByLink(string link)
+        {
+            return Task.Run(()=>_dbContext.Links.FirstOrDefault(x => x.Link.Equals(link)).Id);
+        }
+        #region Disposable
+
+        // ReSharper disable once InconsistentNaming
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+
+                disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        ~Unit{
+            Dispose(false);
+        }
+        #endregion
+    }
+}
