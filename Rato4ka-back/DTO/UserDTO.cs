492d9841c785ceafab8511be7322015e6f3b1a9a
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-10-28 01:40:56 +0300

    Created Users DTO's

---
+++ Rato4ka-back/DTO/UserDTO.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+using Rato4ka_back.Models;
+
+namespace Rato4ka_back.DTO
+{
+
+    public record UserDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? DiscordId { get; set; }
+        public bool IsAdmin { get; set; }
+        public UserDTO()
+        {
+
+        }
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            DiscordId = user.DiscordId;
+            IsAdmin = user.IsAdmin;
+        }
+        public static implicit operator User(UserDTO user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                Name = user.Name,
+                IsAdmin = user.IsAdmin,
+                DiscordId = user.DiscordId
+            };
+        }
+    }
+}
---
+++ Rato4ka-back/DTO/UserDetailInfoDTO.cs
@@ -0,0 +1,35 @@
+﻿using Rato4ka_back.Models;
+
+namespace Rato4ka_back.DTO
+{
+    public record UserDetailInfoDTO: UserDTO
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public UserDetailInfoDTO()
+        {
+
+        }
+        public UserDetailInfoDTO(User user)
+            :base(user)
+        {
+            Login = user.Login;
+            Email = user.Email;
+            IsEmailConfirmed = user.Confirmed;
+        }
+        public static implicit operator User(UserDetailInfoDTO user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                Login = user.Login,
+                Email = user.Email,
+                Name = user.Name,
+                IsAdmin = user.IsAdmin,
+                Confirmed = user.IsEmailConfirmed,
+                DiscordId = user.DiscordId
+            };
+        }
+    }
+}
