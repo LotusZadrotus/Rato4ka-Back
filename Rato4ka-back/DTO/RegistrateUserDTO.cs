3969550c22624474c655ace77066b2f9382ad445
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-10-28 22:32:45 +0300

    DTO for registration

---
+++ Rato4ka-back/DTO/RegistrateUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using Rato4ka_back.Models;
+
+namespace Rato4ka_back.DTO
+{
+    public record RegistrateUserDTO: UserDetailInfoDTO
+    {
+        public string Password { get; set; }
+        public byte[] Avatar { get; set; }
+        public RegistrateUserDTO(User user)
+            :base(user)
+        {
+            Password = user.Password;
+            Avatar = user.Avatar;
+        }
+        public RegistrateUserDTO()
+        {
+
+        }
+    }
+}
