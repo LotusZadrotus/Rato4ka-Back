ffa9084098052edd18b2b6d852549c641def1f4e
Nikita <nikitamikhalchenko@gmail.com>
2023-09-06 17:12:43 +0300

    Modified IRepositoty interface

--- Rato4ka-back/Repositories/IRepository.cs
+++ Rato4ka-back/Repositories/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Rato4ka_back.Repositories{
     public interface IRepository<T> where T: Base{
         Task<T> GetAsync(int id);
-        Task<IEnumerable<T>> GetListAsync();
+        IAsyncEnumerable<T> GetListAsync();
         Task<T> AddAsync(T item);
         Task DeleteAsync(T item);
         Task DeleteAsync(int id);
